7730c6d1cea1e59b13e1a81a272218d593f7d0ed
samukaaguiar <samuel@linqcode.com.br>
2022-12-09 11:52:55 -0300

    Primieiro commit

---
+++ HelloWord.js
@@ -0,0 +1,11 @@
+/* Primeira Aula, somente hello word */
+console.log("Hello word the Javascript");
+
+/* Segunda Aula falando sobre variaveis */
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const idadeSomada = idade + 2;
+
+console.log("Idade: " + idade);
+console.log("Idade Somada: " + idadeSomada);
