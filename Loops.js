c85174aed7f1466a7caecaca8ee2a990081f1a33
samukaaguiar <samuel@linqcode.com.br>
2022-12-09 17:37:47 -0300

    Atualizando

--- Loops.js
+++ Loops.js
@@ -10,26 +10,38 @@ listaDestinos.push(`Curitiba`);
 const idadeComprador = 15;
 const estaAcompanhada = false;
 const passagemComprada = true;
-const destino = `Sergipe`;
+const destino = `Curitiba`;
 
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
-let contador = 0;
 let destinoExiste = false;
 
+// Usando o while
+let contador = 0;
 while(contador < listaDestinos.length){
     if(listaDestinos[contador] == destino){
         destinoExiste = true;
         break;
     }
-    
+
     contador++;
 }
 
 if(destinoExiste)
-    console.log(`Destino existe`);
+    console.log(`Destino existe (Usando o While)`);
 else
-    console.log(`Destino Não existe`);
+    console.log(`Destino Não existe (Usando o While)`);
 
+destinoExiste = false;
+// Usando o for
+for(let cont = 0;cont < listaDestinos.length; cont++)
+    if(listaDestinos[cont] == destino)
+        destinoExiste = true;
+
+            
+if(destinoExiste)
+    console.log(`Destino existe (Usando o for)`);
+else
+    console.log(`Destino Não existe (Usando o for)`);
 
 
